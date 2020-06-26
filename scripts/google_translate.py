a367e25118a40bd954abc6fe1125daa0c9602243
SND96 <sahithdn@gmail.com>
2020-06-26 13:53:08 -0400

    Updated translations

---
+++ scripts/google_translate.py
@@ -0,0 +1,25 @@
+from google.cloud import translate_v2 as translate
+import six
+
+translate_client = translate.Client()
+
+with open('../data/langlist.txt') as f:
+    langs = f.read().strip().split('\n')
+    
+typ = "jobs"
+with open('../data/' + typ + '.txt') as f:
+    text = f.read().strip().split('\n')
+
+if isinstance(text, six.binary_type):
+    text = text.decode('utf-8')
+
+
+for lang in langs:
+    print(lang)
+    with open('../data/' + typ + '/' + lang + '.txt', 'w', encoding="utf-8") as f:
+        result = translate_client.translate(
+            text, target_language=lang)
+
+        for word in result:
+            f.write(word['translatedText']+'\n')
+
